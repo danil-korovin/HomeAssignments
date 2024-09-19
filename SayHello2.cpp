99dcabeea905e2bb51bbf8ac837b00b29dfc4c8e
danil-korovin <st128737@student.spbu.ru>
2024-09-19 18:48:07 +0300

    Создаём функцию

---
+++ SayHello2.cpp
@@ -0,0 +1,20 @@
+/* Korovin Danil st128737@student.spbu.ru
+   problem 1
+*/
+#include<iostream>
+#include<string>
+
+
+using namespace std;
+
+
+
+void SayHello2(){
+	cout << "Hello, world!" << endl;
+	
+	while(true){
+		string x;
+		cin >> x;
+		cout << "Hello, " << x << "!" << endl;
+	}
+}
