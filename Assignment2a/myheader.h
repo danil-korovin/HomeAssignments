a9389ddc1b3b7c88551ecf7345237ca3d03dc7e0
danil-korovin <st128737@student.spbu.ru>
2024-10-04 01:29:05 +0300

    add header

---
+++ Assignment2a/myheader.h
@@ -0,0 +1,11 @@
+/* Korovin Danil st128737@student.spbu.ru
+   Assignment 2a
+*/
+#ifndef myheader_h
+#define myheader_h
+#include <iostream>
+#include <fstream>
+
+void func(const std::string &s);
+
+#endif
