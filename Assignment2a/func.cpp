76b0be03bee13da56aebca79bf952aebaf846d28
danil-korovin <st128737@student.spbu.ru>
2024-10-11 02:34:09 +0300

    updating func.cpp

--- Assignment2a/func.cpp
+++ Assignment2a/func.cpp
@@ -4,22 +4,21 @@
 #include "myheader.h"
 void func()
 {
-    std::ifstream infile;
+	std::ifstream infile;
 	infile.open("source",std::ios::binary|std::ios::in);
 	
 	std::size_t fileSize = std::filesystem::file_size("source");
 	
-	char* buffer = new char[fileSize];	
-		
+	char* buffer = new char[fileSize];
+	
 	std::ofstream outfile;
 	outfile.open("temppdf",std::ios::binary|std::ios::out);
 	infile.read(buffer, fileSize);
 	std::reverse(buffer, buffer + fileSize);
 	outfile.write(buffer, fileSize);
-		 
 	
 	infile.close();
 	outfile.close();
-
+	
 	delete[] buffer;
 }
