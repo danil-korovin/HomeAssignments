dfb5ada33bb27402be65ae767e9db0b4c495f5fb
danil-korovin <st128737@student.spbu.ru>
2024-10-11 02:11:41 +0300

    add myheader.h

---
+++ Assignment2b/myheader.h
@@ -0,0 +1,13 @@
+/* Korovin Danil st128737@student.spbu.ru
+   Assignment 2b
+*/
+#ifndef myheader_h
+#define myheader_h
+
+#include <string>
+#include <iostream>
+#include <stack>
+#include <sstream>
+int func(const std::string &name);
+
+#endif
