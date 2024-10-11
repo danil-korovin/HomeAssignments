dc19ab3162f18a92bf64fe6b7968e6d5bf61e794
danil-korovin <st128737@student.spbu.ru>
2024-10-11 18:12:35 +0300

    updating func.cpp

--- Assignment2b/func.cpp
+++ Assignment2b/func.cpp
@@ -3,48 +3,54 @@
 */
 #include "myheader.h"
 
-int func(const std::string &name) {
-	std::stack<int> stack;
+double func(const std::string &name) {
+	size_t size = 10;
+	double *sp = new double[size];
+	size_t i = 0;
 	std::istringstream iss(name);
 	std::string st;
 	while (iss >> st) 
 	{
 		if (st == "+")
 		{
-			int x = stack.top();
-			stack.pop();
-			int y = stack.top();
-			stack.pop();
-			stack.push(y + x);
+			double x = sp[--i];
+			double y = sp[--i];
+			sp[i++] = x + y;
 		}
 		else if (st == "-")
 		{
-			int x = stack.top();
-			stack.pop();
-			int y = stack.top();
-			stack.pop();
-			stack.push(y - x);
+			double x = sp[--i];
+			double y = sp[--i];
+			sp[i++] = y - x;
 		}
 		else if (st == "*")
 		{
-			int x = stack.top();
-			stack.pop();
-			int y = stack.top();
-			stack.pop();
-			stack.push(y * x);
+			double x = sp[--i];
+			double y = sp[--i];
+			sp[i++] = x * y;
 		}
 		else if (st == "/")
 		{
-			int x = stack.top();
-			stack.pop();
-			int y = stack.top();
-			stack.pop();
-			stack.push(y / x);
+			double x = sp[--i];
+			double y = sp[--i];
+			sp[i++] = y / x;
 		}
 		else
 		{
-			stack.push(std::stoi(st));
+			sp[i++] = std::stod(st);
+		}
+		if (i == size)
+		{
+			size += 1;
+			double *sp2 = new double[size];
+			for (size_t j = 0; j < i; ++j){
+				sp2[j] = sp[j];
+			}
+			delete[] sp;
+			sp = sp2;
 		}
 	}
-	return stack.top();
+	double ans = sp[0];
+	delete[] sp;
+	return ans;
 }
